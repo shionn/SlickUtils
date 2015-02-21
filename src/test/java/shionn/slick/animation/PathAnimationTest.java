44ef90d3c7a550c33d4a66a309ba05d63ed675b9
Kepka Ludovic <shionn@gmail.com>
2015-02-21 11:13:28 +0100

    test unitaire pour animation LOOP

--- src/test/java/shionn/slick/animation/PathAnimationTest.java
+++ src/test/java/shionn/slick/animation/PathAnimationTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.newdawn.slick.geom.Vector2f;
 
+import shionn.slick.animation.PathAnimation.Mode;
+
 /**
  * Code sous licence GPLv3 (http://www.gnu.org/licenses/gpl.html)
  *
@@ -22,13 +24,30 @@ public class PathAnimationTest {
 	}
 
 	@Test
-	public void testStart() throws Exception {
+	public void testStartOnce() throws Exception {
 		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 0));
-		animation.start();
+		animation.start(Mode.ONCE);
 		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 0));
 		animation.update(50);
 		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(50, 75));
 		animation.update(1000);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 100));
+		animation.update(42);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 100));
+	}
+
+	@Test
+	public void testStartLOOP() throws Exception {
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 0));
+		animation.start(Mode.LOOP);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 0));
+		animation.update(50);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(50, 75));
+		animation.update(50);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 100));
+		animation.update(25);
+		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(37.5f, 43.75f));
+		animation.update(75);
 		assertThat(animation.currentLocation()).isEqualTo(new Vector2f(0, 100));
 	}
 
